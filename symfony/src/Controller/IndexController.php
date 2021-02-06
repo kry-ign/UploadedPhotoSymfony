1de14d03138daeecc7f0d503f97dd24d999e0aa3
kry.ign93 <kris_ignatiuk@wp.pl>
2021-02-06 14:34:38 +0100

    create UploadedPhotoType.php

--- symfony/src/Controller/IndexController.php
+++ symfony/src/Controller/IndexController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Photo;
+use App\Form\UploadedPhotoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,11 +13,30 @@ class IndexController extends AbstractController
 {
     /**
      * @Route("/", name="index")
+     * @param Request $request
+     * @return Response
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(UploadedPhotoType::class);
+        $em = $this->getDoctrine()->getManager();
+
+        $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                if ($this->getUser()) {
+                    $entityPhotos = new Photo();
+                    $entityPhotos->setFilename($form->get('filename')->getData());
+                    $entityPhotos->setIsPublic($form->get('is_public')->getData());
+                    $entityPhotos->setUploadedAt(new \DateTime());
+                    $entityPhotos->setUser($this->getUser());
+
+                    $em->persist($entityPhotos);
+                    $em->flush();
+                }
+            }
+
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'form' => $form->createView(),
         ]);
     }
 }
